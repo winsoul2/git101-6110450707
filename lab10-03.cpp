568fab2061f9dc2edd17fb042ffa78912855a60d
Jessadakron Manee <jessadakron.m@ku.th>
2019-10-28 17:13:23 +0700

    Add lab10-03.cpp

---
+++ lab10-03.cpp
@@ -0,0 +1,25 @@
+lass Circle{
+private:
+    point p;
+    double radius;
+public:
+    Circle(point p2, double r){
+        this->p.xPosition = p2.xPosition;
+        this->p.yPosition = p2.yPosition;
+        this->radius = r;
+    }
+
+    double area(){
+    return M_PI*pow(this->radius, 2);
+    }
+
+    double distanceFromCenter(point pt){
+        return sqrt(pow(pt.xPosition - p.xPosition, 2) + pow(pt.yPosition - p.yPosition, 2));
+    }
+
+    int contains(point pt){
+        double count = sqrt(pow(pt.xPosition - p.xPosition, 2) + pow(pt.yPosition - p.yPosition, 2));
+        if(count > this->radius) return 0;
+        else return 1;
+    }
+};
